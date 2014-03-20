03bd0a91c3f8e48f21ca3005de9b46d4af4002c5
Samir Lavingia <lavingiasa@gmail.com>
2014-03-20 18:01:54 +0400

    Added a basic skeleton for the Car class

---
+++ FinalProject/src/Cars/Car.java
@@ -0,0 +1,20 @@
+package Cars;
+
+public class Car 
+{
+	private int id;
+	private double speed;
+	private String direction;
+	private String ramp;
+	private String freeway;
+	
+	public Car(int id, double speed, String direction, String ramp, String freeway)
+	{
+		this.id = id;
+		this.speed = speed;
+		this.direction = direction;
+		this.ramp = ramp;
+		this.freeway = freeway;
+	}
+	
+}
