96a975cc3a29dc9bee13a106977734834cff58f5
Alexei Naumann <anaumann@usc.edu>
2014-04-07 20:21:49 -0700

    Parsed .txt file and added visible waypoints for 110 that appear on the map

--- FinalProject/src/Map/MapGUI.java
+++ FinalProject/src/Map/MapGUI.java
@@ -4,6 +4,7 @@ import java.awt.Cursor;
 import java.awt.Point;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
@@ -220,7 +221,23 @@ public class MapGUI extends JFrame implements JMapViewerEventListener
 	}
 
 	private void testFreeqwayWaypoints() {
-		
-		map().addMapMarker(new MapMarkerDot(xLocation, yLocation));
+		try {
+			FileReader fr = new FileReader("src/FreewayWaypoints.txt");
+			BufferedReader br = new BufferedReader(fr);
+			String line;
+			String headerText = br.readLine();
+			System.out.println("Adding points on " + headerText);         // Prints out name of highway
+			while ((line = br.readLine()) != null) {
+				final String[] coordinatesArray = line.split(",");
+				final double x = Double.parseDouble(coordinatesArray[0]);
+				final double y = Double.parseDouble(coordinatesArray[1]);
+//				System.out.println(x + ", " + y);
+				map().addMapMarker(new MapMarkerDot(x, y));
+			}
+			
+			br.close();
+		} catch (NumberFormatException nfe) { nfe.printStackTrace(); } 
+		catch (FileNotFoundException e) { e.printStackTrace(); }  
+		catch (IOException e) { e.printStackTrace();}		
 	}
 }
