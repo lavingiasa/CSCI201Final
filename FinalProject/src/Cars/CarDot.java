bade1936b0b12f9cc204b96923b75e77cc70db10
Samir Lavingia <lavingiasa@gmail.com>
2014-04-08 13:37:06 -0700

    Got different colours for the dots :)

--- FinalProject/src/Cars/CarDot.java
+++ FinalProject/src/Cars/CarDot.java
@@ -1,11 +1,15 @@
 package Cars;
 
+import java.awt.AlphaComposite;
 import java.awt.Color;
+import java.awt.Composite;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Point;
 
 import org.openstreetmap.gui.jmapviewer.Coordinate;
 import org.openstreetmap.gui.jmapviewer.Layer;
 import org.openstreetmap.gui.jmapviewer.MapMarkerCircle;
-import org.openstreetmap.gui.jmapviewer.MapMarkerDot;
 import org.openstreetmap.gui.jmapviewer.Style;
 
 public class CarDot extends MapMarkerCircle{
@@ -46,5 +50,29 @@ public class CarDot extends MapMarkerCircle{
     {
         return new Style(Color.BLACK, Color.YELLOW, null, getDefaultFont());
     }
+    
+	@Override
+	public void paint(Graphics g, Point position, int radio) 
+	{
+		//super.paint(arg0, arg1, arg2);
+		int size_h = radio;
+        int size = size_h * 2;
+
+        if (g instanceof Graphics2D && getBackColor()!=null) {
+            Graphics2D g2 = (Graphics2D) g;
+            Composite oldComposite = g2.getComposite();
+            g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER));
+            //g2.setPaint(getBackColor());
+            g2.setPaint(Color.BLUE);
+            g.fillOval(position.x - size_h, position.y - size_h, size, size);
+            g2.setComposite(oldComposite);
+        }
+        g.setColor(getColor());
+        g.drawOval(position.x - size_h, position.y - size_h, size, size);
+
+        if(getLayer()==null||getLayer().isVisibleTexts()) paintText(g, position);
+	}
+    
+    
 
 }
--- FinalProject/src/Map/MapGUI.java
+++ FinalProject/src/Map/MapGUI.java
@@ -32,6 +32,7 @@ import org.openstreetmap.gui.jmapviewer.interfaces.TileSource;
 import org.openstreetmap.gui.jmapviewer.tilesources.MapQuestOsmTileSource;
 
 import Cars.Car;
+import Cars.CarDot;
 import Freeways.Interstate10;
 import Freeways.Interstate101;
 import Freeways.Interstate105;
@@ -185,7 +186,7 @@ public class MapGUI extends JFrame implements JMapViewerEventListener
 	//http://svn.openstreetmap.org/applications/viewer/jmapviewer/src/org/openstreetmap/gui/jmapviewer/
 	private void drawTheRampOnTheMap(Double xLocation, Double yLocation) 
 	{
-		map().addMapMarker(new MapMarkerDot(Color.PINK, xLocation, yLocation));
+		map().addMapMarker(new CarDot(Color.PINK, xLocation, yLocation));
 
 	}
 	
